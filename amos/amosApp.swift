76fe1112d501d7b678ca37d54f5058f4c64ba684
Francesco Crivelli <crivelli@berkeley.edu>
2023-09-22 19:49:20 -0700

    Initial Commit

---
+++ amos/amosApp.swift
@@ -0,0 +1,17 @@
+//
+//  amosApp.swift
+//  amos
+//
+//  Created by Francesco Crivelli on 9/22/23.
+//
+
+import SwiftUI
+
+@main
+struct amosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
